4f1512928669207492ec39f791f7d8873e59341a
SurgicalSteel <morpheusgun@outlook.com>
2015-12-16 20:27:22 +0700

    update the stream_test.py file, with console input may contains spaces

--- Streaming Twitter Data in Python using tweepy/scripts/stream_test.py
+++ Streaming Twitter Data in Python using tweepy/scripts/stream_test.py
@@ -4,20 +4,24 @@ from tweepy.streaming import StreamListener
 from tweepy import Stream
 import json
 import tweepy
+import sys
 auth = tweepy.OAuthHandler("8FFhYU4Uf2uNxqB2PWDCkOF3y","za03dIX7yR1L9ZBzTVDbnBzK4A3tA7EJzmyY909vgAeVbg8cGB")
 auth.set_access_token("206679796-MWUsxrgOU0K6USKX5AvC12J0GlwZ9lRVxO3VD67m","IPg2fOqq0u558isQn9WYYuOGxORimf76hWy0ZiGOayHYm")
 api = tweepy.API(auth)
 
 class mylistener(StreamListener):
+    i=1
     def on_data(self,data):
         mydata=json.loads(data) #as known before that 'data' is a string type variable, so I can just perform json.loads
-        print (mydata['user']['name']+" (@"+mydata['user']['screen_name']+") : "+mydata['text'])
+        print (str(self.i)+". "+mydata['user']['name']+" (@"+mydata['user']['screen_name']+") : "+mydata['text'])
         #print(data)
+        self.i+=1
         return True
     def on_error(self,status):
         print(status)
 
 if __name__=='__main__':
+    inp=sys.stdin.readline()
     listen=mylistener()
     stream=Stream(auth,listen)
-    stream.filter(track=['Yogyakarta']) #use 'Yogyakarta' as keyword
+    stream.filter(track=[inp]) #use a single variable as keyword
